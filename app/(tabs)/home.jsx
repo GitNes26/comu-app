9a54df8c026a944d001b6cd56ad6d6f771e2c2de
GitNes26 <nestorpuentesin@gmail.com>
2024-07-09 22:24:38 -0600

    se creo el hook useFetch y se creo un listado de Cards con un pequeño icono, titulo subbtitulo y una imagen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -12,28 +12,40 @@ import images from "../../constants/images";
 import SearchComponent from "../../components/SearchComponent";
 import CarruselComponent from "../../components/CarruselComponent";
 import EmptyComponent from "../../components/EmptyComponent";
+import PreviewCardComponent from "../../components/PreviewCardComponent";
+import useFetch from "../../hooks/useFetch";
+import { getAllPhotos, getAllPosts } from "../../contexts/GlobalContext";
 
 const Home = () => {
+   const {
+      data: photos,
+      isLoading,
+      refetch: refetchPhotos,
+   } = useFetch(getAllPhotos);
+   console.log("🚀 ~ Home ~ photos:", photos);
+   const { data: users, refetch: refetchUsers } = useFetch(getAllPhotos);
+   // const videos = [
+   //    { id: "cards.png", image: "cards" },
+   //    { id: "profile.png", image: "profile" },
+   //    { id: "thumbnail.png", image: "thumbnail" },
+   // ];
    const [refreshing, setRereshing] = useState(false);
 
    const onRefresh = async () => {
       setRereshing(true);
-      // Haz lo tuyoooo
-      setTimeout(() => {
-         ToastAndroid.show("Se actualizo", ToastAndroid.SHORT);
-         setRereshing(false);
-      }, 1000);
+      await refetchPhotos();
+      await refetchUsers();
+      ToastAndroid.show("Se actualizo", ToastAndroid.SHORT);
+      setRereshing(false);
    };
 
    return (
-      <SafeAreaView className={"bg-primary"}>
+      <SafeAreaView className={"bg-primary h-full"}>
          <FlatList
-            data={[]}
-            // data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+            // data={[]}
+            data={photos}
             // keyExtractor={({ item }) => item.id}
-            renderItem={({ item }) => (
-               <Text className={"text-3xl text-white"}>{item.id}</Text>
-            )}
+            renderItem={({ item }) => <PreviewCardComponent video={item} />}
             ListHeaderComponent={() => (
                <View className={"my-6 px-4 space-y-6"}>
                   <View className={"justify-between items-start flex-row"}>
